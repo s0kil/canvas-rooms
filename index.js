92465394adc332aa6a693602e13edd210e473835
Daniel Sokil <dtechtours@gmail.com>
2021-03-09 21:01:33 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+// import { createState, createEffect } from "solid-js"
+import { render } from "solid-js/web"
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+render(App, document.querySelector("main"))
